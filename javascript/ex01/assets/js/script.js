0aad1bb36d9d0664c0b7f329879fa9dffe1ea968
Karolynemirella <karolynemirella03@gmail.com>
2025-04-15 01:43:06 -0300

    .

---
+++ javascript/ex01/assets/js/script.js
@@ -0,0 +1,7 @@
+let nome = "Karol"
+let idade = 100
+let logado = true
+let lista = ["ovo", "massa", "corante"]
+let nomeCompleto = { nome: "Bonueky", idade: 100 }
+
+console.log(typeof nomeCompleto)
